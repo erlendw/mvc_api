9d9c4fba17d9adc2ada4a480964902339a687f71
erlendw <erlendwestbye@gmail.com>
2015-11-11 16:47:59 +0100

    bundleconfig med mystisk error

---
+++ TeaShopFaq/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace TeaShopFaq.App_Start
+{
+        public class BundleConfig
+        {
+            // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+            public static void RegisterBundles(BundleCollection bundles)
+            {
+                bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                            "~/Scripts/jquery-{version}.js"));
+
+                bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                          "~/Scripts/bootstrap.js",
+                          "~/Scripts/respond.js"));
+
+                bundles.Add(new StyleBundle("~/Content/css").Include(
+                          "~/Content/bootstrap.css",
+                          "~/Content/site.css",
+                          "~/Content/basic.css",
+                           "~/Content/dropzone.css",
+                           "~/Content/font-awesome.css"));
+
+                bundles.Add(new ScriptBundle("~/bundles/dropzonescripts").Include(
+                         "~/Scripts/dropzone/dropzone.js"));
+
+                bundles.Add(new ScriptBundle("~/bundles/theteashop").Include(
+                         "~/Scripts/theteashop.js"));
+            }
+        }
+
+    }
+}
